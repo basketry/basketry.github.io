7e522a14f74a5f6c93103b0573867a1a8f177301
Steve Konves <stephen.konves@gmail.com>
2024-05-29 11:48:06 -0700

    Update social card (#13)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -68,7 +68,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/basketry-social-card.png",
     navbar: {
       title: "Basketry",
       logo: {
